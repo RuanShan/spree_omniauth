1e39c132255bff8355b4e53b14e6ee7cfbcd4f49
david <areq22@aliyun.com>
2017-02-25 14:15:34 +0800

    fix bug signup_path

--- app/controllers/spree/open/wechats_controller.rb
+++ app/controllers/spree/open/wechats_controller.rb
@@ -23,8 +23,8 @@ module Spree::Open
         sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
         set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
       else
-        session["devise.facebook_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
+        session["devise.wechat_data"] = request.env["omniauth.auth"]
+        redirect_to spree.signup_path
       end
     end
 
